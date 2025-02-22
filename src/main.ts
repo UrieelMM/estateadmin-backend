92b946721db4ca0fea71f46d316f0366035a678c
urieeldev <uriel.mejia@corebiz.ag>
2025-02-21 20:11:17 -0600

    Feat: Se cambia configuracion de los CORS

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Configuración de CORS para permitir solicitudes desde http://localhost:5173
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
