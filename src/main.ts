5c46159f97ae1f599c49f5c00e10a727793e350f
UrielDev <urieel.mm@gmail.com>
2025-03-25 15:21:36 -0600

    Feat: Update files

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Habilitar CORS
   app.enableCors({
-    origin: ['http://localhost:5174', 'http://localhost:5173','http://localhost:5174'], // Orígenes permitidos
+    origin: [
+      'http://localhost:5174',
+      'http://localhost:5173',
+      'https://estate-admin.com',
+    ], // Orígenes permitidos
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
